fabf7ab82e6f56054ac9ad16931bd9eeaa70a46a
Nguyen Lan Nhi <162776742+luquorus@users.noreply.github.com>
2024-12-06 10:14:42 +0700

    10k database

---
+++ 10K/theaters.sql
@@ -0,0 +1,6 @@
+INSERT INTO Theater (Theater_id, Name, Address, City, Total_Rooms) VALUES
+(1, 'Velez Ltd', '66166 Robert Street West Darren, AR 74512', 'Lake Robert', 14),
+(2, 'Anderson Ltd', '98536 Kelly Mall Apt. 493 Ericbury, MS 63983', 'North Stephanie', 15),
+(3, 'Johnson Inc', '68728 Jefferson Glens Suite 759 Lake John, OR 24864', 'South Rebecca', 12),
+(4, 'Aguirre-Moore', '6733 Shari Ranch Suite 595 Turnerton, PA 48467', 'Barnestown', 15),
+(5, 'Contreras, White and Keller', '3252 Katherine Place East Jessicafurt, TN 32714', 'Gatesburgh', 16);
---
+++ 10K/vouchers.sql
@@ -0,0 +1,11 @@
+INSERT INTO Voucher (Voucher_id, Description, Discount_Percentage, Expiry_Date, PointsRequired) VALUES
+(1, 'Discount for 10%', 10, '2020-09-20 00:00:00', 10),
+(2, 'Discount for 20%', 20, '2021-12-10 00:00:00', 20),
+(3, 'Discount for 25%', 25, '2021-11-26 00:00:00', 25),
+(4, 'Discount for 30%', 30, '2023-09-28 00:00:00', 30),
+(5, 'Discount for 35%', 35, '2024-09-11 00:00:00', 35),
+(6, 'Discount for 40%', 40, '2024-12-03 00:00:00', 40),
+(7, 'Discount for 50%', 50, '2022-06-09 00:00:00', 50),
+(8, 'Discount for 55%', 55, '2022-10-10 00:00:00', 55),
+(9, 'Discount for 60%', 60, '2022-05-02 00:00:00', 60),
+(10, 'Discount for 70%', 70, '2022-01-31 00:00:00', 70);
